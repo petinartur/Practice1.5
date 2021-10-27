b7c87d9a70ba5a22e7307775035fa4d63f13313d
Artur <petin.art@gmail.com>
2021-10-27 16:51:53 +0300

    Initial Commit

---
+++ Practice1.5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice1.5
+//
+//  Created by Артур Петин on 27.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
